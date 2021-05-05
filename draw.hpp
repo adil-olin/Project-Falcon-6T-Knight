dc6d79545898d263cc923089d2a81e409bd612f0
Shah Nawaz Parvez <69872274+snpovi4@users.noreply.github.com>
2021-05-05 14:04:36 +0600

    great balls of fireeeeeeeee

--- draw.hpp
+++ draw.hpp
@@ -9,12 +9,8 @@ using namespace std;
 
 void prepareScene(void)
 {
-	SDL_SetRenderDrawColor(app.renderer, 0, 100, 100, 255);
+	SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
 	SDL_RenderClear(app.renderer);
-	SDL_Texture* texture = IMG_LoadTexture(app.renderer,"background.jpg");
-	SDL_RenderCopy(app.renderer,texture,NULL,NULL);
-	SDL_DestroyTexture(texture);
-	texture = NULL;
 }
 
 void presentScene(void)
@@ -25,13 +21,11 @@ void presentScene(void)
 SDL_Texture *loadTexture(char *filename)
 {
 	SDL_Texture *texture;
-	SDL_Surface* surface;
+
 	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-	surface = IMG_Load(filename);
-	SDL_SetColorKey(surface,SDL_TRUE,SDL_MapRGB(surface->format,0x00,0x00,0x00));
-	texture = SDL_CreateTextureFromSurface(app.renderer,surface);
-	SDL_FreeSurface(surface);
-	surface = NULL;
+
+	texture = IMG_LoadTexture(app.renderer, filename);
+
 	return texture;
 }
 
@@ -63,7 +57,12 @@ static void drawenemy(void)
 	{
 		blit(tmp.texture, tmp.x, tmp.y);
 	}
+	for (auto tmp : destroyed_enemy)
+	{
+		blit(tmp.texture, tmp.x, tmp.y);
+	}
 }
+
 static void draw(void)
 {
 	drawPlayer();
